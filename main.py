051b7ec8046ff0006f56b1bb3b454e2ef9e1c4ec
vinodmp4 <vinodmp4@live.com>
2023-02-09 13:31:43 +0530

    begin testing

--- main.py
+++ main.py
@@ -4,4 +4,17 @@ import gl
 import graphics as gp
 
 
-print(fp.get_filename())
+
+filename = fp.get_filename('SN','AS',1)
+inputfile = fp.File_Processor(filename)
+content = inputfile.read()
+text_obj = dp.Text('sanskrit',content)
+no_digits = text_obj.remove_digit()
+#no_symbols = text_obj.remove_symbol(no_digits)
+#sentence_only = text_obj.get_sentence(no_digits)
+no_headings = text_obj.remove_headings(no_digits)
+blocks = inputfile.split(no_headings, 40, False, False)
+
+print(blocks[0])
+
+
